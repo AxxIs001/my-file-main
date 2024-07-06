c550337e105a12096e227fd11a97d10bd1438cfa
AxxIs001 <411021385@gms.ndhu.edu.tw>
2024-07-06 18:28:32 +0800

    .

--- Javascript/practice and projects/Notes/notes.js
+++ Javascript/practice and projects/Notes/notes.js
@@ -3,6 +3,16 @@ const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 const deleteBtn = document.querySelector(".delete")
 
+function showNodes() {
+    noteContainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNodes();
+
+function updateStorage() {
+    localStorage.setItem("notes", noteContainer.innerHTML);
+}
+
 createBtn.addEventListener("click", () => {
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -10,4 +20,30 @@ createBtn.addEventListener("click", () => {
     inputBox.setAttribute("contenteditable", "true");
     img.src = "img/delete.png";
     noteContainer.appendChild(inputBox).appendChild(img);
-});
+})
+
+noteContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentNode.remove()
+        updateStorage();
+    } else if (e.target.tagName == "P") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function() {
+                updateStorage();
+            }
+        })
+    }
+
+})
+
+//It is for preventing an error, like when there is more lines the 2nd 
+//and others will be out of the input-box, so it is for preventing it;
+
+document.addEventListener("keydown", event => {
+    if (event.key === "Enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+
+})
